8ca86bdc95337e6a6b7a8be8359ef9ea8146bcb2
haxn8 <natenatenat3@protonmail.com>
2025-05-29 21:26:58 +0800

    we're able to format the output nicely :P

--- src/backend/exec.rs
+++ src/backend/exec.rs
@@ -1,3 +1,29 @@
 // httpp.rs - Nathanael "NateNateNate" Thevarajah
 // <natenatenat3@protonmail.com> - Refer to the license for more
 // information.
+
+use crate::backend::parser::Request;
+
+pub async fn send_request(req: Request) -> (reqwest::StatusCode, String) {
+    let client = reqwest::Client::new();
+
+    let mut request_builder = client.request(req.method.parse().unwrap(), &req.path);
+
+    for (k, v) in &req.headers {
+        request_builder = request_builder.header(k, v);
+    }
+
+    if let Some(body) = &req.body {
+        request_builder = request_builder.json(&body);
+    }
+
+    let response = request_builder
+        .send()
+        .await
+        .expect("Failed to send request");
+
+    let status = response.status();
+    let text = response.text().await.expect("Failed to read response body");
+
+    (status, text)
+}
